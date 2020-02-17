87e6847ab1a4372ea8975fd2449c71bedf1e7cea
Alexandre Muniz <aom@puzzlezapper.com>
2020-02-17 14:22:47 -0800

    It passes one test

---
+++ __tests__/presentation.test.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Presentation = require('../lib/models/Presentation');
+
+describe('dance routes', () => {
+  
+  beforeAll(() => {
+    connect();
+  });
+  
+  // let examplePresentation;
+  beforeEach(async() => {
+    await mongoose.connection.dropDatabase();
+
+    await Presentation.create({
+      title: 'History of Morris Dance',
+      presenter: 'Linda G',
+      description: '1000 Years of Morris',
+      isScheduled: false,
+    });
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('gets all presentations', async() => {
+    return request(app)
+      .get('/api/v1/presentation/')
+      .then(res => {
+        console.log(res.body);
+        return expect(res.body).toEqual(
+          [
+            {
+              __v: 0,
+              _id: expect.any(String),
+              title: 'History of Morris Dance',
+              presenter: 'Linda G',
+              description: '1000 Years of Morris',
+              isScheduled: false,
+            }
+          ] 
+        );
+      });
+  });
+});
